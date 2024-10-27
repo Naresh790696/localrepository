fad27b7f17f261165bb9b53ac4f4dc58be0fdfd0
narendra <nilesh9012345456@gmail.com>
2024-10-27 19:46:47 +0530

    changes are done

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let a = ["rahul","rehaman","parul", "sohan","papa","rehaman"];
+
+let b = a.lastIndexOf("rehaman");
+let c = a.lastIndexOf("rehaman",2);
+
+document.write(b)
+"&nbsp;"
+document.write(c)
+
+
